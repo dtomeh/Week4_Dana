7b309d964354543ebeb116e17da04f87c68219b9
dtomeh <tomeh001@umn.edu>
2020-02-12 15:01:22 -0600

    Data Import and CLeaning 2

--- R/Week4 Code.R
+++ R/Week4 Code.R
@@ -1,3 +1,12 @@
 #R Studio API Code
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+
+#Data Import
+library(tidyverse)
+week4_df <- read_delim("../Data/week4.dat", delim ="-", col_names= c("casenum", "parnum", "stimver", "datadate", "qs"))
+glimpse(week4_df)
+week4_df <- separate(week4_df, qs, c("q1", "q2", "q3", "q4", "q5"))
+week4_df[5:9] <- sapply(week4_df[5:9], as.numeric)
+week4_df <- complete(week4_df, )
+week4_df[, 5:9][week4_df[ 5:9]==0] <-NA
